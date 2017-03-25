ae3c257dd7934b82738b050658b65e9097a00d1d
Han Yuan <Han Yuan>
2017-03-25 12:16:54 +0000

    update

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def verify():
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
-    return "Hello world, edit 2", 200
+    return "Hello world, edit 3", 200
 
 
 @app.route('/', methods=['POST'])
--- master.py
+++ master.py
@@ -5,7 +5,7 @@ Created on Mon Mar 20 09:50:19 2017
 @author: alexr
 """
 import sys
-from pywit.wit.wit import Wit
+from wit1 import Wit
 
 
 #print(sys.argv)
