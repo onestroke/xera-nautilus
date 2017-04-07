737ba4ac538cb4b5e4239de1a2f4b531964d3e7c
Han Yuan <Han Yuan>
2017-04-07 03:08:37 +0100

    upd

--- master.py
+++ master.py
@@ -42,7 +42,7 @@ def wit_msg(sender_id,message_text):
     print('appending to logs.txt')
     file = open('logs.txt','a') 
     log=(sender_id, message_text)
-    file.write(a)
+    file.write(log)
     file.close
     
     def log(message):  # simple wrapper for logging to stdout on heroku
