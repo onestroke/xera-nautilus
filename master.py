1a1a02d4bb6dce66c76a4dcda76cacad532916be
Han Yuan <alexrider0820@hotmail.com>
2017-06-29 02:16:53 +0800

    debug

--- master.py
+++ master.py
@@ -57,7 +57,7 @@ def wit_msg(sender_id,message_text):
     def getFullTest(request):
         context = request['context']
         entities = request['entities']
-        try: fd = load('FullDiagnostic.txt')
+        try: fd = load('contacts.txt')
         except ValueError:
             fd=[]
         print(fd)
